c4e9e8ec17879fae222b5bfa419a138bb8eaaaf1
bw8 <>
2022-05-02 22:48:07 +0200

    First steps to configure lualine with xresources.
    Using colors from xresources plugin to set colors.
    Added fallback colors if xresources isn't available.

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -1,3 +1,21 @@
+-- Xresources colors
+local status_ok, xcolors = pcall(require, 'xresources')
+if not status_ok then
+    local xcolors = {
+        black        = '#282828',
+        white        = '#ebdbb2',
+        red          = '#fb4934',
+        green        = '#b8bb26',
+        blue         = '#83a598',
+        yellow       = '#fe8019',
+        gray         = '#a89984',
+        darkgray     = '#3c3836',
+        lightgray    = '#504945',
+        inactivegray = '#7c6f64',
+    }
+end
+
+-- Lualine
 local status_ok, lualine = pcall(require, 'lualine')
 if not status_ok then
     return
@@ -26,10 +44,36 @@ local filetype = {
 }
 
 
+-- Config
 lualine.setup {
     options = {
         icons_enabled = true,
-        theme = 'auto',
+        -- theme = 'auto',
+        theme = {
+            normal = {
+                a = { fg = xcolors.light_white, bg = xcolors.blue},
+                b = { fg = xcolors.light_white, bg = xcolors.bg},
+                c = { fg = xcolors.light_white, bg = xcolors.black},
+                x = { fg = xcolors.light_white, bg = xcolors.black},
+                y = { fg = xcolors.light_white, bg = xcolors.bg},
+                z = { fg = xcolors.light_white, bg = xcolors.blue}
+            },
+            insert = {
+                a = { fg = xcolors.light_white, bg = xcolors.purple},
+                b = { fg = xcolors.light_white, bg = xcolors.bg},
+                c = { fg = xcolors.white, bg = xcolors.black}
+            },
+            visual = {
+                a = { fg = xcolors.bg, bg = xcolors.cyan},
+                b = { fg = xcolors.light_white, bg = xcolors.bg},
+                c = { fg = xcolors.light_white, bg = xcolors.black}
+            },
+            command = {
+                a = { fg = xcolors.bg, bg = xcolors.yellow},
+                b = { fg = xcolors.white, bg = xcolors.bg},
+                c = { fg = xcolors.white, bg = xcolors.black}
+            },
+        },
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''},
         disabled_filetypes = { "dashboard" },
@@ -37,8 +81,8 @@ lualine.setup {
     },
     sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_b = {'filename'},
+        lualine_c = {'branch', 'diff', 'diagnostics'},
     },
     inactive_sections = {
     },
