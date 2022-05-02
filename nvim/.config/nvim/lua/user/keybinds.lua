a76118b07a64b1567a35901ee71a17ec24afcf3f
bw8 <>
2022-05-02 03:38:47 +0200

    Some lil improvements.
    Now file is divided in sections depending of modes.
    Added silent to all options.

--- nvim/.config/nvim/lua/user/keybinds.lua
+++ nvim/.config/nvim/lua/user/keybinds.lua
@@ -4,15 +4,26 @@ vim.g.mapleader = ' '
 
 -- Keymaps
 -- vim.api.nvim_set_keymap({mode}, {keymap}, {mapped to}, {options})
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true }
-local optss = { noremap = true, silent = true }
 
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+
+-- Mode
+--  normal_mode = 'n'
+--  insert_mode = 'i'
+--  visual_mode = 'v'
+--  visual_block_mode = 'x'
+--  terminal_mode = 't'
+--  command_mode = 'c'
+
+
+-- ================================= Normal ===================================
 -- Move across windows
-keymap('n', '<leader>h', ':wincmd h<CR>', optss)
-keymap('n', '<leader>j', ':wincmd j<CR>', optss)
-keymap('n', '<leader>k', ':wincmd k<CR>', optss)
-keymap('n', '<leader>l', ':wincmd l<CR>', optss)
+keymap('n', '<leader>h', ':wincmd h<CR>', opts)
+keymap('n', '<leader>j', ':wincmd j<CR>', opts)
+keymap('n', '<leader>k', ':wincmd k<CR>', opts)
+keymap('n', '<leader>l', ':wincmd l<CR>', opts)
 
 -- Resize windows
 keymap('n', '<leader>H', ':vertical resize -2<CR>', opts)
@@ -21,18 +32,16 @@ keymap('n', '<leader>K', ':resize -2<CR>', opts)
 keymap('n', '<leader>L', ':vertical resize +2<CR>', opts)
 
 -- Navigate buffers
-keymap('n', '<leader>n', ':bnext<CR>', optss)
-keymap('n', '<leader>p', ':bprevious<CR>', optss)
+keymap('n', '<leader>n', ':bnext<CR>', opts)
+keymap('n', '<leader>p', ':bprevious<CR>', opts)
 
 -- Move across windows
 keymap('n', '<C-j>', ':cnext<CR>', opts)
 keymap('n', '<C-k>', ':cprevious<CR>', opts)
 
--- Open explorer
--- keymap('n', '<leader>f', ':wincmd v<bar> :Ex <bar> :vertical resize 30<CR>', opts)
+-- Open eXplorer
+keymap('n', '<leader>x', '<CMD>Lex 30<CR>', opts)
 
--- -------------------- Spell check --------------------
-keymap('i', '<C-l>', '<c-g>u<Esc>[s1z=`]a<c-g>u', opts) -- <C-l> to correct word
 
 -- -------------------- Skeletons --------------------
 keymap('n', ',html', ':-1read $HOME/.config/nvim/skel/skel.tex', opts)
@@ -45,3 +54,9 @@ keymap('n', '<leader>ff', '<CMD>lua require("telescope.builtin").find_files()<CR
 keymap('n', '<leader>fg', '<CMD>lua require("telescope.builtin").live_grep()<CR>', opts)
 keymap('n', '<leader>fb', '<CMD>lua require("telescope.builtin").buffers()<CR>', opts)
 keymap('n', '<leader>fh', '<CMD>lua require("telescope.builtin").help_tags()<CR>', opts)
+
+
+-- ================================== Insert ==================================
+-- -------------------- Spell check --------------------
+keymap('i', '<C-l>', '<c-g>u<Esc>[s1z=`]a<c-g>u', opts) -- <C-l> to correct word
+
